caa236476a3f75327dd9c6381fe671fd483a88b2
David Ricketts <david.ricketts119@gmail.com>
2022-07-16 12:06:00 -0400

    Header, Footer, and Campsites List

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import CampsitesList from './features/campsites/CampsitesList.js';
+import { CAMPSITES } from './app/shared/CAMPSITES';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      hello nucamp!
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Header />
+            <CampsitesList />
+            <Footer />
+        </div>
+
+    );
 }
 
 export default App;
+
