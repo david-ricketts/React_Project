4a9709f0e589f59d525861a9b3f7cbd837424bac
David Ricketts <david.ricketts119@gmail.com>
2022-07-21 15:57:28 -0400

    Header and Footer Links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,30 @@
-import {Col, Row, Container} from 'reactstrap';
-
-
-
+import { Container, Row, Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
         <footer className='site-footer'>
             <Container>
                 <Row>
-                    <Col xs={{ size: 4, offset: 1 }} sm='2'> 
-                        <h5>this will be site navigation!</h5>
+                    <Col xs={{ size: 4, offset: 1 }} sm='2'>
+                        <h5>Links</h5>
+                        <ul className='list-unstyled'>
+                            <li>
+                                <Link to='/'>Home</Link>
+                            </li>
+                            <li>
+                                <Link to='/directory'>Directory</Link>
+                            </li>
+                            <li>
+                                <Link to='/about'>About</Link>
+                            </li>
+                            <li>
+                                <Link to='/contact'>Contact</Link>
+                            </li>
+                        </ul>
                     </Col>
                     <Col xs='6' sm='3' className='text-center'>
-                    <h5>Social</h5>
+                        <h5>Social</h5>
                         <a
                             className='btn btn-social-icon btn-instagram'
                             href='http://instagram.com/'
@@ -34,10 +46,9 @@ const Footer = () => {
                         <a
                             className='btn btn-social-icon btn-google'
                             href='http://youtube.com/'
-                            >
+                        >
                             <i className='fa fa-youtube' />
                         </a>
-
                     </Col>
                     <Col sm='4' className='text-center'>
                         <a
@@ -55,13 +66,11 @@ const Footer = () => {
                         >
                             <i className='fa fa-envelope-o' /> campsites@nucamp.co
                         </a>
-
                     </Col>
                 </Row>
             </Container>
         </footer>
-    )
-}
-
+    );
+};
 
 export default Footer;
