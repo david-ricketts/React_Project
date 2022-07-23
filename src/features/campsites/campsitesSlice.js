4e42cc856f74272ddffd5ef163f3cf7c03d858d4
David Ricketts <david.ricketts119@gmail.com>
2022-07-23 10:42:14 -0400

    CampsiteDetailPage

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -8,10 +8,14 @@ export const selectAllCampsites = () => {
 //     return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
 // };
 
-export const selectCampsiteById = (id) => {
-    return CAMPSITES.find((campsite) => campsite.id === id);
-};
+// export const selectCampsiteById = (id) => {
+//     return CAMPSITES.find((campsite) => campsite.id === id);
+// };
 
 export const selectFeaturedCampsite = () => {
     return CAMPSITES.find((campsite) => campsite.featured);
+};
+
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
 };
