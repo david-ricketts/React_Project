64897ca070ca4b002d15fc8c518f07d246174667
David Ricketts <david.ricketts119@gmail.com>
2022-08-05 11:59:14 -0400

    Redux campsitesSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+// import counterReducer from '../features/counter/counterSlice';
+import { campsitesReducer } from '../features/campsites/campsitesSlice';
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+    reducer: {
+        campsites: campsitesReducer
+    }
 });
+
+// export const store = configureStore({
+//   reducer: {
+//     counter: counterReducer,
+//   },
+// });
